cde198df6ac21f670f4767282f483a437edb8605
clairemanthey <clairemanthey@gmail.com>
2023-02-13 16:05:51 -0600

    added styles to blog roll and cleaned

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
 
 
 const BlogRollTemplate = (props) => {
@@ -12,43 +11,26 @@ const BlogRollTemplate = (props) => {
     <div className="columns is-multiline">
       {posts &&
         posts.map(({ node: post }) => (
-          <div className="is-parent column is-6" key={post.id}>
+          <div className="post-container" key={post.id}>
             <article
               className={`blog-list-item tile is-child box notification ${
                 post.frontmatter.featuredpost ? 'is-featured' : ''
               }`}
             >
               <header>
-                {post?.frontmatter?.featuredimage && (
-                  <div className="featured-thumbnail">
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: post.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${post.frontmatter.title}`,
-                        width:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.width,
-                        height:
-                          post.frontmatter.featuredimage.childImageSharp
-                            .gatsbyImageData.height,
-                      }}
-                    />
-                  </div>
-                ) }
                 <p className="post-meta">
                   <Link
-                    className="title has-text-primary is-size-4"
+                    className="post-title"
                     to={post.fields.slug}
                   >
                     {post.frontmatter.title}
                   </Link>
-                  <span> &bull; </span>
-                  <span className="subtitle is-size-5 is-block">
+                  <span className="post-subtitle">
                     {post.frontmatter.date}
                   </span>
                 </p>
               </header>
-              <p>
+              <p className="blog-excerpt">
                 {post.excerpt}
                 <br />
                 <br />
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,7 @@
 import * as React from "react";
+import Navbar from "../../components/Navbar"
+import HeaderImage from "../../img/header_image.png";
+
 
 import Layout from "../../components/Layout";
 import BlogRoll from "../../components/BlogRoll";
@@ -7,16 +10,20 @@ export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div className="full-width-image-container margin-top-0">
-          <h1>All Posts</h1>
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
+        <main className="blog-roll-main">
+          <img src={HeaderImage} alt="Header Graphic" />
+          <Navbar />
+          <div className="bolg-roll-header">
+            <h1 className="blog-roll-title center">All Posts</h1>
           </div>
-        </section>
+          <section className="section blog-roll-section">
+            <div className="container blog-roll-container">
+              <div className="content blog-roll-content">
+                <BlogRoll />
+              </div>
+            </div>
+          </section>
+        </main>
       </Layout>
     );
   }
