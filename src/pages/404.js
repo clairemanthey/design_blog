f3b63a64e788ca3cf2f8d6b1d5357bcd9133c024
clairemanthey <clairemanthey@gmail.com>
2023-02-17 11:02:59 -0600

    added 404 with styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,22 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import HeaderImage from "../img/header_image.png";
+import { Link } from "gatsby";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <main className="main-404">
+      <img src={HeaderImage} alt="Header Graphic" />
+      <section className="section-404">
+        <h1 className="title-404">404 NOT FOUND</h1>
+        <h2 className="subtitle-404 center">Find your way back</h2>
+        <article className="link-container-404">
+          <Link to="/"><h3 className="link-404">Home</h3></Link>
+          <Link to="/blog"><h3 className="link-404">Blog</h3></Link>
+          <Link to="/contact"><h3 className="link-404">Contact</h3></Link>
+        </article>
+      </section>
+    </main>
   </Layout>
 );
 
