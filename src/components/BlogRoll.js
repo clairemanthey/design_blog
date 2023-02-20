9acfebaaeb76082af880a3d169877c72c12f0462
clairemanthey <clairemanthey@gmail.com>
2023-02-20 10:30:51 -0600

    updated graphql query on BlogRoll.js

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -74,17 +74,6 @@ export default function BlogRoll() {
                   title
                   templateKey
                   date(formatString: "MMMM DD, YYYY")
-                  featuredpost
-                  featuredimage {
-                    childImageSharp {
-                      gatsbyImageData(
-                        width: 120
-                        quality: 100
-                        layout: CONSTRAINED
-                      )
-
-                    }
-                  }
                 }
               }
             }
